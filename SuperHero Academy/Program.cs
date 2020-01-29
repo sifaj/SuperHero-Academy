f94be089981fb9bb0431469a92d527b95c669c7d
sifaj <54946010+sifaj@users.noreply.github.com>
2020-01-29 14:48:21 +0100

    Inint com

--- SuperHero Academy/Program.cs
+++ SuperHero Academy/Program.cs
@@ -16,7 +16,7 @@ namespace SuperHero_Academy
             Hero h1;
 
             Console.WriteLine("Please choose a hero-category: 1. Tank  2. Ranged  3. Stealthed");
-            input = Choice(1, 3);
+            input = Utils.Choice(1, 3);
 
             if (input == 1)
             {
@@ -39,28 +39,6 @@ namespace SuperHero_Academy
             
         }
 
-        public static int Choice(int minChoice, int maxChoice) // En metod som möjliggör att välja hur många inputs spelaren kan ge, och som ser till att den är giltig inom angett scope.
-        {
-            int returnValue = 0;
-            bool check = false;
-            Console.WriteLine("Choose a number between " + minChoice + " and " + maxChoice + " as input.");
-
-            while (check == false)
-            {
-                int.TryParse(Console.ReadLine(), out returnValue);
-
-                if (returnValue > maxChoice || returnValue < minChoice)
-                {
-                    Console.WriteLine("Not compliable. Choose a number between " + minChoice + " and " + maxChoice + " as input.");
-                    check = false;
-                }
-                else
-                {
-                    return returnValue;
-                }
-            }
-
-            return 1;
-        }
+        
     }
 }
---
+++ SuperHero Academy/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero_Academy
+{
+    class Utils
+    {
+        public static int Choice(int minChoice, int maxChoice) // En metod som möjliggör att välja hur många inputs spelaren kan ge, och som ser till att den är giltig inom angett scope.
+        {
+            int returnValue = 0;
+            bool check = false;
+            Console.WriteLine("Choose a number between " + minChoice + " and " + maxChoice + " as input.");
+
+            while (check == false)
+            {
+                int.TryParse(Console.ReadLine(), out returnValue);
+
+                if (returnValue > maxChoice || returnValue < minChoice)
+                {
+                    Console.WriteLine("Not compliable. Choose a number between " + minChoice + " and " + maxChoice + " as input.");
+                    check = false;
+                }
+                else
+                {
+                    return returnValue;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
